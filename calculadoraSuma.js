344c2586f0dbe6600c791c766862f578a213f6dd
Sergio Rincon <zx21student144@colegiovalledelmiro.es>
2024-10-04 08:36:17 +0200

    Modificada la funcion suma

--- calculadoraSuma.js
+++ calculadoraSuma.js
@@ -1,6 +1,7 @@
 document.getElementById("sumarBtn").addEventListener("click", function() {
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
-    var resultado = num1 + num2;
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let num3 = parseFloat(document.getElementById("num3").value);
+    var resultado = num1 + num2 + num3;
     document.getElementById("resultadoSuma").textContent = resultado;
 });
